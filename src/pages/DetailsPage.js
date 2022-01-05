92784586c21ebc69f387f81053f2baeb73c5d2cc
thevhenz <thevhenz@gmail.com>
2022-01-05 23:53:14 +0700

    Create Parts PageDetailDescription.js

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,31 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Header from 'parts/Header'
-import PageDetailTitle from 'parts/PageDetailTitle'
-import FeaturedImage from 'parts/FeaturedImage'
+import Header from "parts/Header";
+import PageDetailTitle from "parts/PageDetailTitle";
+import FeaturedImage from "parts/FeaturedImage";
+import PageDetailDescription from "parts/PageDetailDescription";
 
-import ItemDetails from "json/itemDetails.json"
+import ItemDetails from "json/itemDetails.json";
 
 export default class DetailsPage extends Component {
-    componentDidMount() {
-        window.title = "Details Page"
-        window.scrollTo(0, 0)
-    }
-    render() {
-        const breadcrumb = [
-            {pageTitle: "Home", pageHref: ""},
-            {pageTitle: "House Details", pageHref: ""}
-        ]
+  componentDidMount() {
+    window.title = "Details Page";
+    window.scrollTo(0, 0);
+  }
+  render() {
+    const breadcrumb = [
+      { pageTitle: "Home", pageHref: "" },
+      { pageTitle: "House Details", pageHref: "" },
+    ];
 
-        return (
-            <>
-              <Header {...this.props}></Header>
-              <PageDetailTitle
-                breadcrumb={breadcrumb}
-                data={ItemDetails}
-              ></PageDetailTitle>
-              <FeaturedImage data={ItemDetails.imageUrls}></FeaturedImage>  
-            </>
-        )
-    }
+    return (
+      <>
+        <Header {...this.props}></Header>
+        <PageDetailTitle
+          breadcrumb={breadcrumb}
+          data={ItemDetails}
+        ></PageDetailTitle>
+        <FeaturedImage data={ItemDetails.imageUrls}></FeaturedImage>
+        <section className="container">
+          <div className="row">
+            <div className="col-7 pr-5">
+              <PageDetailDescription data={ItemDetails}></PageDetailDescription>
+            </div>
+            <div className="col-5">Booking Form</div>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
